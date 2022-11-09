bb8b3de0aa7aba50aba803bf044dd097cd7eaef1
Matthieu Petit <p.matthieu@me.com>
2022-11-09 12:01:54 +0100

    Add 404 page

--- components/layout.tsx
+++ components/layout.tsx
@@ -16,7 +16,7 @@ const Layout = ({ children, className }: React.HTMLAttributes<HTMLElement>) => {
       <Navigation className="max-sm:hidden" />
       <div className="flex w-full flex-col items-stretch justify-start rounded-3xl bg-white shadow-lg sm:flex-row sm:rounded-tr-none">
         <SideBar className="max-sm:rounded-t-none"></SideBar>
-        <main className="overflow-hidden">{children}</main>
+        <main className="grow overflow-hidden">{children}</main>
       </div>
       <Footer className="fixed bottom-0 z-[-1] mb-12 w-full max-w-5xl" />
     </motion.div>
